8a82172c308cf0bc3463b4d8b339df373f0a8809
Daniiarz <daniyarflash.m01@gmail.com>
2020-07-15 13:47:30 +0600

    Added templates view

--- back-end/constructor/urls.py
+++ back-end/constructor/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ConstructorView.as_view(), name="constructor"),
-    path("checkTitle/", views.check_titles, name="check-titles")
+    path("checkTitle/", views.check_titles, name="check-titles"),
+    path("templates/", views.TemplatesView.as_view(), name="templates")
 ]
